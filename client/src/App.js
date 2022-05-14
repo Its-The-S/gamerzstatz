82a19c3cb8529b876fcf2f3e917a3d4e80c7402b
bgould86 <bgould86@gmail.com>
2022-05-14 08:18:01 -0700

    harded coded chart component works

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
+import Chart from "./components/Chart/Chart";
 
 // switch this to react provider
 // import { Provider } from "react-redux";
@@ -13,42 +14,43 @@ import Signup from "./pages/Signup";
 import Nav from "./components/Nav";
 
 const httpLink = createHttpLink({
-    uri: "/graphql",
+  uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
-    const token = localStorage.getItem("id_token");
-    return {
-        headers: {
-            ...headers,
-            authorization: token ? `Bearer ${token}` : "",
-        },
-    };
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
 });
 
 const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
 });
 
 function App() {
-    return (
-        <ApolloProvider client={client}>
-            <Router>
-                <div>
-                    {/* <Provider store={store}> */}
-                    <Nav />
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/login" element={<Login />} />
-                        <Route path="/signup" element={<Signup />} />
-                        <Route path="*" element={<NoMatch />} />
-                    </Routes>
-                    {/* </Provider> */}
-                </div>
-            </Router>
-        </ApolloProvider>
-    );
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <Chart />
+        <div>
+          {/* <Provider store={store}> */}
+          <Nav />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="*" element={<NoMatch />} />
+          </Routes>
+          {/* </Provider> */}
+        </div>
+      </Router>
+    </ApolloProvider>
+  );
 }
 
 export default App;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,10 @@
 import React from "react";
-
 const Home = () => {
-    return <div className="container">HELLO WORLD</div>;
+  return (
+    <>
+      <div className="container">Chart</div>
+    </>
+  );
 };
 
 export default Home;
