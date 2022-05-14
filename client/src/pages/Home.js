803fdff37106ff451fc9377d1a7da560f50bcea9
bgould86 <bgould86@gmail.com>
2022-05-14 11:40:22 -0700

    made page to display chart for now

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,15 @@
 import React from "react";
 const axios = require("axios");
 // const xbl = axios.create({
-//     baseURL: "http://xbl.io/api/v2",
-//     headers: { "X-Authorization": "wsscs0cswwgkg4s4ko40804o40s0444kckc", "Access-Control-Allow-Origin": "*", "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS", "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Requested-With" },
-//     responseType: "json",
+//   baseURL: "http://xbl.io/api/v2",
+//   headers: { "X-Authorization": "wsscs0cswwgkg4s4ko40804o40s0444kckc", "Access-Control-Allow-Origin": "*", "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS", "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Requested-With" },
+//   responseType: "json",
 // });
 
 const Home = () => {
-    const account = axios.get("/api/account").then((res) => console.log(res));
+  const account = axios.get("/api/account").then((res) => console.log(res));
 
-    return <div className="container">HELLO WORLD</div>;
+  return <div className="container">HELLO WORLD</div>;
 };
 
 export default Home;
