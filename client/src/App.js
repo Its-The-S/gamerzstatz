008e4bdb2dfeb4147bc5308115e01adc5aaf005f
joepamedia <josephpalmisano18@gmail.com>
2022-05-17 19:55:00 -0500

    added footer base

--- client/src/App.js
+++ client/src/App.js
@@ -11,46 +11,48 @@ import Signup from "./pages/Signup";
 import Profile from "./pages/Profile";
 import Nav from "./components/Nav";
 import ChartForNow from "./pages/ChartForNow";
+import Footer from "./components/Footer/index";
 
 const httpLink = createHttpLink({
-    uri: "/graphql",
+  uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
-    const token = localStorage.getItem("id_token");
-    return {
-        headers: {
-            ...headers,
-            authorization: token ? `Bearer ${token}` : "",
-        },
-    };
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
 });
 
 const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
 });
 
 function App() {
-    return (
-        <ApolloProvider client={client}>
-            <UserProvider>
-                <Router>
-                    <div>
-                        <Nav />
-                        <Routes>
-                            <Route path="/" element={<Home />} />
-                            <Route path="/login" element={<Login />} />
-                            <Route path="/signup" element={<Signup />} />
-                            <Route path="/profile" element={<Profile />} />
-                            <Route path="*" element={<NoMatch />} />
-                            <Route path="/chart" element={<ChartForNow />} />
-                        </Routes>
-                    </div>
-                </Router>
-            </UserProvider>
-        </ApolloProvider>
-    );
+  return (
+    <ApolloProvider client={client}>
+      <UserProvider>
+        <Router>
+          <div>
+            <Nav />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="*" element={<NoMatch />} />
+              <Route path="/chart" element={<ChartForNow />} />
+            </Routes>
+            <Footer />
+          </div>
+        </Router>
+      </UserProvider>
+    </ApolloProvider>
+  );
 }
 
 export default App;
