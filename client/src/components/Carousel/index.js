00689d660d86e3311c8602e380f9dec5021e26e7
Maya Tillman <mayatillman17@gmail.com>
2022-05-19 18:36:50 -0500

    splides

---
+++ client/src/components/Carousel/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
+//hard code images
+import Sims from "../../assets/sims.png";
+import Cod from "../../assets/cod.jpg";
+import Pvz from "../../assets/pvz.jpg";
+import Minecraft from "../../assets/Minecraft_cover.png";
+
+export default function Carousel() {
+  return (
+    <Splide aria-label="My Favorite Images">
+      <SplideSlide>
+        <img src={Cod} alt="Call of Duty" />
+      </SplideSlide>
+      <SplideSlide>
+        <img src={Sims} alt="Sims" />
+      </SplideSlide>
+      <SplideSlide>
+        <img src={Pvz} alt="Plants vs Zombies" />
+      </SplideSlide>
+      <SplideSlide>
+        <img src={Minecraft} alt="Minecraft" />
+      </SplideSlide>
+    </Splide>
+  );
+}
