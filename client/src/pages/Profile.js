2c48e5df0804b25bc090cd778fca71a528c89423
Aidan Chamberlain <asunshine99@gmail.com>
2022-05-18 22:53:18 -0500

    updated profile page with card array and profile data onscreen

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,17 +1,8 @@
 import React, { useEffect, useState } from "react";
 import Card from "../components/Card/index";
-// import { useUser } from "../utils/UserContext";
-// const axios = require("axios");
+const cardMaxIndex = 9;
 
 const Profile = () => {
-    // const [account, getAccount] = useState("");
-    // const { currentUser, fetchUserAccount } = useUser();
-    // let profileData = {};
-
-    // useEffect(() => {
-    //     profileData = JSON.parse(localStorage.getItem("user"));
-    // }, []);
-
     const [profileData, setProfileData] = useState({});
     const [achieveData, setAchieveData] = useState({});
 
@@ -23,30 +14,39 @@ const Profile = () => {
         setAchieveData(allAchievements);
     }, []);
 
-    // const fetchAccount = async () => {
-    //     try {
-    //         const fetch = await axios.get("/api/account");
-    //         console.log("fetch data:", fetch.data);
-    //         // getAccount(fetch.data);
-    //         setCurrentUser(fetch.data);
-    //         console.log(currentUser);
-    //     } catch (err) {
-    //         console.error(err);
-    //     }
-    // };
-
-    // if (account.length !== 0) {
-    //     console.log(account["profileUsers"][0].settings[2].value);
-    // }
-
-    return (
-        <div className="container">
-            {/* {currentUser.length !== 0 ? <h2>Hello, {currentUser["profileUsers"][0].settings[2].value}</h2> : <h2>Loading</h2>} */}
-            {/* {profileData.length !== 0 ? <h2>Hello, {profileData.gamertag}</h2> : <h2>Loading</h2>} */}
-            <h2>Hello, {profileData.gamertag}</h2>
-            <Card />
-        </div>
-    );
+    if (achieveData.titles !== undefined && achieveData.titles.length > 0) {
+        return (
+            <div className="container">
+                {/* {currentUser.length !== 0 ? <h2>Hello, {currentUser["profileUsers"][0].settings[2].value}</h2> : <h2>Loading</h2>} */}
+                {/* {profileData.length !== 0 ? <h2>Hello, {profileData.gamertag}</h2> : <h2>Loading</h2>} */}
+                <div>
+                    <img src={profileData.avatar} alt="profile avatar" />
+                    <h2>{profileData.gamertag}</h2>
+                    <h4>Gamerscore: {profileData.gamerscore}</h4>
+                </div>
+                {/* {achieveData.length !== undefined ? (
+                <div>
+                    {achieveData.titles.map((game) => {
+                        <Card />;
+                    })}
+                </div>
+            ) : (
+                <h2>Loading...</h2>
+            )} */}
+                {/* <Card />; */}
+                <div>
+                    {achieveData.titles.map((game, index) => {
+                        if (index <= cardMaxIndex) {
+                            return <Card />;
+                        } else {
+                            return <></>;
+                        }
+                    })}
+                </div>
+            </div>
+        );
+    }
+    return <></>;
 };
 
 export default Profile;
