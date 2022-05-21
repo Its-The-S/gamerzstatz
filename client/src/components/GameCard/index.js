dc3824dcdef3147a3610c283c35b13bcdc6839a7
Aidan Chamberlain <asunshine99@gmail.com>
2022-05-21 15:36:26 -0500

    test updated card

--- client/src/components/GameCard/index.js
+++ client/src/components/GameCard/index.js
@@ -1,58 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Card from "react-bootstrap/Card";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-// import Button from "react-bootstrap/Button";
 
 export default function GameCard(props) {
-  const cardStyle = {
-    width: "18rem",
-    border: "solid grey 5px",
-    padding: "20px",
-    margin: "30px",
-  };
+    const cardStyle = {
+        width: "18rem",
+        border: "solid grey 5px",
+        padding: "20px",
+        margin: "30px",
+    };
 
-  //button will be used
-  return (
-    <div className="container">
-      <div className="card" style={cardStyle}>
-        <h5 className="card-title">{props.title}</h5>
-        <img className="card-img-top" src={props.coverArt} alt="Card cap" />
-        <div className="card-body">
-          <p>Completed Achievements: {props.currentAchievements}</p>
-          <p>
-            Gamerscore: {props.currentGamerScore} / {props.totalGamerScore}
-          </p>
-          <p>Percent Complete: {props.percentAchieve}%</p>
-          <Link to={`/compare/${props.titleId}`}>Check My Stats</Link>
+    return (
+        <div className="container">
+            <div className="card" style={cardStyle}>
+                <h5 className="card-title">{props.title}</h5>
+                <img className="card-img-top" src={props.coverArt} alt="Card cap" />
+                <div className="card-body">
+                    <p>Completed Achievements: {props.currentAchievements}</p>
+                    <p>
+                        Gamerscore: {props.currentGamerScore} / {props.totalGamerScore}
+                    </p>
+                    <p>Percent Complete: {props.percentAchieve}%</p>
+                    <Link to={`/compare/${props.titleId}`}>Check My Stats</Link>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
-
-//   return (
-//     <Row xs={1} md={3} className="g-6">
-//       {Array.from({ length: 1 }).map((_, idx) => (
-//         <Col>
-//           <Card style={cardStyle}>
-//             <Card.Img variant="top" src={props.coverArt} />
-//             <Card.Body>
-//               <Card.Title>{props.title}</Card.Title>
-//               <Card.Text>
-//                 <p>Completed Achievements: {props.currentAchievements}</p>
-//                 <p>
-//                   Gamerscore: {props.currentGamerScore} /{" "}
-//                   {props.totalGamerScore}
-//                 </p>
-//                 <p>Percent Complete: {props.percentAchieve}%</p>
-//               </Card.Text>
-//               <Link to={`/compare/${props.titleId}`}>Check My Stats</Link>
-//             </Card.Body>
-//           </Card>
-//         </Col>
-//       ))}
-//     </Row>
-//   );
-// }
