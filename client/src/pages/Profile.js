4ccbac5ebdc0538252799378eee8d473e2bfb339
Aidan Chamberlain <asunshine99@gmail.com>
2022-05-21 14:53:18 -0500

    checking joe push error

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -20,7 +20,7 @@ const Profile = () => {
                 <div>
                     {/* <i className="fa-brands fa-xbox"></i> */}
 
-                    {/* <img src={profileData.avatar} alt="profile avatar" /> */}
+                    <img src={profileData.avatar} alt="this user's profile avatar" />
                     <h2>{profileData.gamertag}</h2>
                     <h4>Gamerscore: {profileData.gamerscore}</h4>
                 </div>
