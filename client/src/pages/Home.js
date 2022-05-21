6c6e58cf3108de90f5bd2be2daa11c1f023b239c
joepamedia <josephpalmisano18@gmail.com>
2022-05-21 15:20:18 -0500

    style fix

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,10 @@
 import React, { useEffect, useState } from "react";
-// import Card from "../components/Card/index";
 import Carousel from "../components/Carousel";
 
 const Home = () => {
   return (
     <div className="container">
       <Carousel />
-      {/* <Card /> */}
     </div>
   );
 };
