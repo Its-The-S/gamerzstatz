fd785fa4b32a6ac6ea0d0ac3eec3ce1955268abf
bgould86 <bgould86@gmail.com>
2022-05-23 18:57:42 -0700

    fixed the footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,66 +2,66 @@ import React from "react";
 
 // footer element
 export default function Footer() {
-    return (
-        <div>
-            <footer className="df footer">
-                {/* footer card for joe */}
-                <div className="footer-card lg">
-                    <h6>Joe Palmisano</h6>
-                    <a href="https://www.linkedin.com/in/joseph-palmisano-jnp/" className="" target="_blank">
-                        <i className="fa fa-linkedin px-1" />
-                    </a>
-                    <a href="https://github.com/joepamedia" target="_blank">
-                        <i className="fa fa-github px-1" />
-                    </a>
-                </div>
-                {/* footer card for aidan */}
-                <div className="footer-card lg">
-                    <h6>Aidan Chamberlain</h6>
-                    <a href="https://www.linkedin.com/in/aidan-chamberlain-776b8b90/" className="" target="_blank">
-                        <i className="fa fa-linkedin px-1" />
-                    </a>
-                    <a href="https://github.com/RedComet6" target="_blank">
-                        <i className="fa fa-github px-1" />
-                    </a>
-                </div>
-                {/* footer card for brett */}
-                <div className="footer-card lg">
-                    <h6>Brett Gould</h6>
-                    <a href="https://www.linkedin.com/in/brettdgould//" className="" target="_blank">
-                        <i className="fa fa-linkedin px-1" />
-                    </a>
-                    <a href="https://github.com/bgould86" target="_blank">
-                        <i className="fa fa-github px-1" />
-                    </a>
-                </div>
-                {/* footer card for maya */}
-                <div className="footer-card lg">
-                    <h6>Maya Tillman</h6>
-                    <a href="https://www.linkedin.com/in/maya-tillman-430084227/" className="" target="_blank">
-                        <i className="fa fa-linkedin px-1" />
-                    </a>
-                    <a href="https://github.com/mayaatil" target="_blank">
-                        <i className="fa fa-github px-1" />
-                    </a>
-                </div>
-                {/* footer card for tom */}
-                <div className="footer-card lg">
-                    <h6>Thomas Frohm</h6>
-                    <a href="https://www.linkedin.com/in/thomas-f-767203232/" target="_blank">
-                        <i className="fa fa-linkedin px-1" />
-                    </a>
-                    <a href="https://github.com/tfrohm11" target="_blank">
-                        <i className="fa fa-github px-1" />
-                    </a>
-                </div>
-                <div className="footer-card sm">
-                    <h6>Its The S</h6>
-                    <a href="https://github.com/Its-The-S/gamerzstatz" target="_blank">
-                        <i className="fa fa-github px-1" />
-                    </a>
-                </div>
-            </footer>
+  return (
+    <div>
+      <footer className="df footer">
+        {/* footer card for joe */}
+        <div className="footer-card lg">
+          <h6>Joe Palmisano</h6>
+          <a href="https://www.linkedin.com/in/joseph-palmisano-jnp/" className="" target="_blank">
+            <i className="fa fa-linkedin px-1" />
+          </a>
+          <a href="https://github.com/joepamedia" target="_blank">
+            <i className="fa fa-github px-1" />
+          </a>
         </div>
-    );
+        {/* footer card for aidan */}
+        <div className="footer-card lg">
+          <h6>Aidan Chamberlain</h6>
+          <a href="https://www.linkedin.com/in/aidan-chamberlain-776b8b90/" className="" target="_blank">
+            <i className="fa fa-linkedin px-1" />
+          </a>
+          <a href="https://github.com/RedComet6" target="_blank">
+            <i className="fa fa-github px-1" />
+          </a>
+        </div>
+        {/* footer card for brett */}
+        <div className="footer-card lg">
+          <h6>Brett Gould</h6>
+          <a href="https://www.linkedin.com/in/brettdgould//" className="" target="_blank">
+            <i className="fa fa-linkedin px-1" />
+          </a>
+          <a href="https://github.com/bgould86" target="_blank">
+            <i className="fa fa-github px-1" />
+          </a>
+        </div>
+        {/* footer card for maya */}
+        <div className="footer-card lg">
+          <h6>Maya Tillman</h6>
+          <a href="https://www.linkedin.com/in/maya-tillman-430084227/" className="" target="_blank">
+            <i className="fa fa-linkedin px-1" />
+          </a>
+          <a href="https://github.com/mayaatil" target="_blank">
+            <i className="fa fa-github px-1" />
+          </a>
+        </div>
+        {/* footer card for tom */}
+        <div className="footer-card lg">
+          <h6>Thomas Frohm</h6>
+          <a href="https://www.linkedin.com/in/thomas-f-767203232/" target="_blank">
+            <i className="fa fa-linkedin px-1" />
+          </a>
+          <a href="https://github.com/tfrohm11" target="_blank">
+            <i className="fa fa-github px-1" />
+          </a>
+        </div>
+        <div className="footer-card sm">
+          <h6>Its The S</h6>
+          <a href="https://github.com/Its-The-S/gamerzstatz" target="_blank">
+            <i className="fa fa-github px-1" />
+          </a>
+        </div>
+      </footer>
+    </div>
+  );
 }
